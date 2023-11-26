346c4594622434ac867e2d9bd21f6763c2ee1fbc
Lloyd Tano <Lloyd Tano@DESKTOP-6JPKDDG>
2023-11-27 06:23:03 +0800

    Created Controller and ViewModel for Register, Login and Logout.

---
+++ pixelBooru/Models/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace pixelBooru.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "a username is required")]
+        public string? Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "a password is required")]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginInfo)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginInfo.Username,
+                loginInfo.Password, loginInfo.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Instructor");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Failed to Login");
+            }
+
+            return View(LoginInfo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signinManager.SignOutAsync();
+            return RedirectToAction("Index", "Instructor");
+        }
+
+
+
+    }
+}
